0c54b7f486b5563325216c1017071d102adaadd4
Ivaylo Stoyanov <ivaylo@evolutionofdreams.com>
2026-03-17 15:49:27 +0200

    Add tailwind cs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonMenu,
+  IonList,
+  IonItem,
+  IonMenuToggle,
+  IonButtons,
+  IonMenuButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import {
+  businessOutline,
+  schoolOutline,
+  informationCircleOutline,
+  pricetagsOutline,
+  callOutline,
+  logoGooglePlaystore,
+  logoApple,
+  checkmarkCircle,
+  mapOutline,
+  locationOutline,
+  mailOutline,
+  logoLinkedin,
+  logoTwitter,
+  logoFacebook,
+  logoYoutube,
+} from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonContent,
+    IonMenu,
+    IonList,
+    IonItem,
+    IonMenuToggle,
+    IonButtons,
+    IonMenuButton,
+    IonIcon,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      businessOutline,
+      schoolOutline,
+      informationCircleOutline,
+      pricetagsOutline,
+      callOutline,
+      logoGooglePlaystore,
+      logoApple,
+      checkmarkCircle,
+      mapOutline,
+      locationOutline,
+      mailOutline,
+      logoLinkedin,
+      logoTwitter,
+      logoFacebook,
+      logoYoutube,
+    });
+  }
 }
